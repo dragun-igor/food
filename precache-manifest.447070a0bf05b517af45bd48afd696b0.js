df6c8eda0d075b6b98e1822ecf3e589bbb42701a
Igor Dragun <draggun.ia@gmail.com>
2022-09-15 09:57:53 +0300

    Updates

---
+++ precache-manifest.447070a0bf05b517af45bd48afd696b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec5499b06d8d7127f3a5",
+    "url": "/food/static/css/main.baa916ab.chunk.css"
+  },
+  {
+    "revision": "ec5499b06d8d7127f3a5",
+    "url": "/food/static/js/main.ec5499b0.chunk.js"
+  },
+  {
+    "revision": "f82b6b0fa5ceeb100c49",
+    "url": "/food/static/js/1.f82b6b0f.chunk.js"
+  },
+  {
+    "revision": "67fe68913997935f0b4c",
+    "url": "/food/static/js/runtime~main.67fe6891.js"
+  },
+  {
+    "revision": "fdd6f31fb269c9806ab1b204aaf92169",
+    "url": "/food/index.html"
+  }
+];
